2f047e80768025e571f2b132e471d4cf2e4f8fab
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-04-01 08:57:37 -0300

    Middleware

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -7,6 +7,22 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found' });
+  }
+
+  request.customer = customer;
+
+  return next();
+
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
@@ -28,15 +44,11 @@ app.post('/account', (request, response) => {
   return response.status(201).send();
 });
 
-app.get('/statement', (request, response) => {
-  const { cpf } = request.headers;
+// app.use(verifyIfExistsAccountCPF);
 
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: 'Customer not found' });
-  }
-
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  
   return response.json(customer.statement);
 })
 
