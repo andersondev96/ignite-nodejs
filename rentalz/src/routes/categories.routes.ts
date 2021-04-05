caea82f4ee4bdba1ccf462877f9597d673ee35dd
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-04-05 08:43:25 -0300

    Criando tipagem por categoria

--- rentalz/src/routes/categories.routes.ts
+++ rentalz/src/routes/categories.routes.ts
@@ -1,17 +1,24 @@
 import { Router } from "express";
 
+import { Category } from "../model/Category";
+
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = new Category();
+
+  Object.assign(category, {
     name,
     description,
+    created_at: new Date(),
   });
 
-  return response.status(201).send();
+  categories.push(category);
+
+  return response.status(201).json({ category });
 });
 export { categoriesRoutes };
