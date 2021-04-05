02616065b486eed71d4e66b93a4d57f34323502f
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-04-05 08:25:51 -0300

    Criando categoria

--- rentalz/src/server.ts
+++ rentalz/src/server.ts
@@ -1,14 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 
-app.get("/", (request, response) => response.json({ message: "Hello World" }));
-
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-  return response.json({ name });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
