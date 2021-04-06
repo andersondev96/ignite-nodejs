c0a0e39c3ee86db93d5a4a70dd502915066de596
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-04-06 08:44:20 -0300

    Utilizando o Princípio de Substituição de Liskov (LSP)

--- rentalz/src/repositories/CategoriesRepository.ts
+++ rentalz/src/repositories/CategoriesRepository.ts
@@ -1,19 +1,17 @@
 import { Category } from "../model/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "./ICategoriesRepository";
 
-// DTO - Data transfer object
-interface ICreateCategoryDTO {
-  name: string;
-  description: string;
-}
-
-class CategoriesRepository {
+class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
   constructor() {
     this.categories = [];
   }
 
-  create({ description, name }: ICreateCategoryDTO): void {
+  create({ name, description }: ICreateCategoryDTO): void {
     const category = new Category();
 
     Object.assign(category, {
