abe3151d6802ae7ffa3153725888504a37fbdb14
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-04-07 08:48:23 -0300

    Criando os Use Case de categoria

--- rentalz/src/routes/specification.routes.ts
+++ rentalz/src/routes/specification.routes.ts
@@ -7,15 +7,6 @@ const specificationsRoutes = Router();
 
 const specificationsRepository = new SpecificationsRepository();
 
-specificationsRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  return response.status(201).send();
-});
+specificationsRoutes.post("/", (request, response) => {});
 
 export { specificationsRoutes };
