cf8b4b144e604a5b86c680754dcd30cdc1690a92
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-04-21 16:13:53 -0300

    Criando a migration de especificação

--- rentx/src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ rentx/src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -13,8 +13,9 @@ class CreateSpecificationUseCase {
     @inject("SpecificationsRepository")
     private specificationsRepository: ISpecificationsRepository
   ) {}
-  execute({ name, description }: IRequest): void {
-    const specificationAlreadyExists = this.specificationsRepository.findByName(
+
+  async execute({ name, description }: IRequest): Promise<void> {
+    const specificationAlreadyExists = await this.specificationsRepository.findByName(
       name
     );
 
@@ -22,7 +23,7 @@ class CreateSpecificationUseCase {
       throw new Error("Specification already exists!");
     }
 
-    this.specificationsRepository.create({
+    await this.specificationsRepository.create({
       name,
       description,
     });
