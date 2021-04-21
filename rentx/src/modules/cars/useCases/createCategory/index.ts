89b2ef1525aab40fd1f909fbe11f255a7d908385
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-04-21 14:48:23 -0300

    Conhecendo TSyringe

--- rentx/src/modules/cars/useCases/createCategory/index.ts
+++ rentx/src/modules/cars/useCases/createCategory/index.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+/* import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { CreateCategoryController } from "./CreateCategoryController";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
@@ -13,3 +13,4 @@ export default (): CreateCategoryController => {
 
   return createCategoryController;
 };
+ */
