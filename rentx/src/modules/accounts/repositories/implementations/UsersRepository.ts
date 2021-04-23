2a072ffed88615124bc63cd097e8b80732986bc9
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-04-23 08:19:05 -0300

    Criptografar senha

--- rentx/src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ rentx/src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -10,6 +10,7 @@ class UsersRepository implements IUsersRepository {
   constructor() {
     this.repository = getRepository(User);
   }
+
   async create({
     name,
     email,
@@ -24,6 +25,11 @@ class UsersRepository implements IUsersRepository {
     });
 
     await this.repository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({ email });
+    return user;
   }
 }
 
