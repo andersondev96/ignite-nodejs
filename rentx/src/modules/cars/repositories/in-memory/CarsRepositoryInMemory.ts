e45c0da908d9afde9d66445b16769526317da208
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-04-29 09:05:38 -0300

    TDD na prática

---
+++ rentx/src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -0,0 +1,33 @@
+import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDTO";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+
+import { ICarsRepository } from "../ICarsRepository";
+
+class CarsRepositoryInMemory implements ICarsRepository {
+  cars: Car[] = [];
+  async create({
+    brand,
+    category_id,
+    daily_rate,
+    description,
+    fine_amount,
+    name,
+    license_plate,
+  }: ICreateCarDTO): Promise<void> {
+    const car = new Car();
+
+    Object.assign(car, {
+      brand,
+      category_id,
+      daily_rate,
+      description,
+      fine_amount,
+      name,
+      license_plate,
+    });
+
+    this.cars.push(car);
+  }
+}
+
+export { CarsRepositoryInMemory };
