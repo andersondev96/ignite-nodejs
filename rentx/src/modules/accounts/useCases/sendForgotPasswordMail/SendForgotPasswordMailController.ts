758ad165bca400b4cc8772f2bfe710f63e9e3510
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-05-22 21:44:13 -0300

    Criando o provider de e-mail

--- rentx/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController.ts
+++ rentx/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController.ts
@@ -4,7 +4,7 @@ import { container } from "tsyringe";
 import { SendForgotPasswordMailUseCase } from "./SendForgotPasswordMailUseCase";
 
 class SendForgotPasswordMailController {
-  async execute(request: Request, response: Response): Promise<Response> {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { email } = request.body;
 
     const sendForgotPasswordMailUseCase = container.resolve(
--- rentx/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ rentx/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -4,6 +4,7 @@ import { v4 as uuidV4 } from "uuid";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUsersTokenRepository } from "@modules/accounts/repositories/IUsersTokenRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 import { AppError } from "@shared/errors/AppError";
 
 @injectable()
@@ -14,9 +15,12 @@ class SendForgotPasswordMailUseCase {
     @inject("UsersTokenRepository")
     private usersTokensRepository: IUsersTokenRepository,
     @inject("DayjsDateProvider")
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+    @inject("EtherealMailProvider")
+    private mailProvider: IMailProvider
   ) {}
-  async execute(email: string) {
+
+  async execute(email: string): Promise<void> {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
@@ -32,6 +36,12 @@ class SendForgotPasswordMailUseCase {
       user_id: user.id,
       expires_date,
     });
+
+    await this.mailProvider.sendMail(
+      email,
+      "Recuperação de senha",
+      `O link para o reset é ${token}`
+    );
   }
 }
 
