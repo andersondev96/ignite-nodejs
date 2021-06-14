eff9b44f55e87d660117c1f8ba8adbc2e4a2b3ca
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-06-14 08:59:54 -0300

    Criando URL de acesso ao avatar

---
+++ rentx/src/modules/accounts/useCases/profileUserUseCase/ProfileUserUseCase.ts
@@ -0,0 +1,22 @@
+import { inject, injectable } from "tsyringe";
+
+import { IUserResponseDTO } from "@modules/accounts/dtos/IUserResponseDTO";
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
+import { UserMap } from "@modules/accounts/mapper/UserMap";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+
+@injectable()
+class ProfileUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute(id: string): Promise<IUserResponseDTO> {
+    const user = await this.usersRepository.findById(id);
+
+    return UserMap.toDTO(user);
+  }
+}
+
+export { ProfileUserUseCase };
